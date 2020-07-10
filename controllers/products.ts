f7b3ba78e137cde7f72e158b7e0bdfd0b42a699e
norictech <ahmad.uji1902@gmail.com>
2020-07-10 21:53:40 +0700

    product model & data

---
+++ controllers/products.ts
@@ -0,0 +1,22 @@
+import { Products } from '../types.ts'
+
+let products: Products[] = [
+    {
+        id: "1",
+        name: "Product One",
+        description: "This is product one",
+        price: 29.99,
+    },
+    {
+        id: "2",
+        name: "Product Two",
+        description: "This is product two",
+        price: 39.99,
+    },
+    {
+        id: "3",
+        name: "Product Three",
+        description: "This is product three",
+        price: 49.99,
+    },
+]
